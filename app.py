38742931ea0c958376d0d8213ef6dfa01868c7a0
ChaimaOkbi <104589656+ChaimaOkbi@users.noreply.github.com>
2025-06-05 00:19:18 +0200

    app.py

--- app.py
+++ app.py
@@ -248,8 +248,5 @@ def process_report():
 
 
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', 
-            port=int(os.environ.get('PORT', 5000)), 
-            debug=False)
+    app.run(debug=True)
